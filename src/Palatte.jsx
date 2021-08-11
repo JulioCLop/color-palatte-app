5ce75ffe78cfb927e4b04eeb4d570505d8ce04ce
Julio Lopez <joeyclopez88#gmail.com>
2021-08-10 17:49:13 -0700

    first Commit

---
+++ src/Palatte.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Palatte extends Component {
+    render() {
+        return (
+            <div className="Palette">
+                  {/*navbar goes here*/} 
+               <div className="Palatte-color">
+               {/* bunch of color boxes */}
+                </div>
+                  {/*Footer */}
+            </div>
+        )
+    }
+}
